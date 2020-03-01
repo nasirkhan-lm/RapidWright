b69ab8d07f95873d8c0fe907bb86a89c6ee78e9c
Morten Borup Petersen <morten_bp@live.dk>
2020-03-01 12:27:17 +0100

    Clean up functions w/ functional elements of Kotlin

--- src/com/switchboxscraper/SwitchBoxScraper.kt
+++ src/com/switchboxscraper/SwitchBoxScraper.kt
@@ -5,6 +5,7 @@ import com.xilinx.rapidwright.device.Wire
 
 import guru.nidi.graphviz.attribute.Color
 import guru.nidi.graphviz.attribute.Shape
+import guru.nidi.graphviz.attribute.Style
 import guru.nidi.graphviz.engine.Format
 import guru.nidi.graphviz.engine.Graphviz
 import guru.nidi.graphviz.model.Factory.mutGraph
@@ -15,13 +16,10 @@ import guru.nidi.graphviz.model.MutableNode
 import java.io.File
 import java.util.*
 
-/**
- *
- */
 enum class PJClass {
     CLK,
     ELEC,
-    CONN
+    ROUTING
 }
 
 enum class PJType {
@@ -31,57 +29,61 @@ enum class PJType {
     INTERNAL
 }
 
+enum class PJRoutingType {
+    GLOBAL,
+    CLB,
+    INTERNAL
+}
+
+class PJProperty {
+    var pjClass = PJClass.CLK
+    var routingType = PJRoutingType.GLOBAL
+    var pjType = PJType.SOURCE
+}
+
 class SwitchBoxScraper {
     var pipJunctions = mutableSetOf<Wire>()
-    var clbPips = mutableListOf<Int>()
-    var llPips = mutableListOf<Int>() // Long lines
-    var name : String = ""
+    var pjClassification = mutableMapOf<Wire, PJProperty>()
+    var name: String = ""
+    var device: Device;
 
-    var device : Device;
-
-    constructor(deviceName : String) {
+    constructor(deviceName: String) {
         device = Device.getDevice(deviceName)
-        scrape()
     }
 
-    fun scrape() {
+    fun scrape(tile: String) {
         // Locate some switchbox tile
-        val sbTile = device.getTile("INT_R_X41Y61")
+        val sbTile = device.getTile(tile)
         this.name = sbTile.name
         println("Scraping Switchox: ${name}")
 
-
         // Extract all pip junctions by iterating over all PIPs in the tile and collecting the pip start/stop wires
         // which are defined as the pip junctions
-        for(pip in sbTile.getPIPs()){
-            pipJunctions.add(pip.startWire)
-            pipJunctions.add(pip.endWire)
-        }
+        sbTile.getPIPs().fold(pipJunctions) { junctions, pip -> junctions.addAll(listOf(pip.startWire, pip.endWire)); junctions }
 
-        var dotFiles = mutableListOf<String>()
-        val graphTypes = listOf(EnumSet.of(PJClass.ELEC), EnumSet.of(PJClass.CONN), EnumSet.of(PJClass.CLK), EnumSet.allOf(PJClass::class.java))
-        for(type in graphTypes) {
-            dotFiles.add(createDotGraph(type))
-        }
+        // Perform PIP junction classification
+        pipJunctions.fold(pjClassification) { acc, pj -> acc[pj] = classifyPJ(pj); acc }
 
-        val wroteFiles = dotFiles + dotsToSvg(dotFiles)
+        val graphTypes = listOf(EnumSet.of(PJClass.ELEC), EnumSet.of(PJClass.ROUTING), EnumSet.of(PJClass.CLK), EnumSet.allOf(PJClass::class.java))
+        var dotFiles = graphTypes.fold(mutableListOf<String>()) { files, type -> files.add(createDotGraph(type)); files }
+
         println("Wrote files:")
-        wroteFiles.map { v -> println("\t${v}") }
+        (dotFiles + dotsToSvg(dotFiles)).map { v -> println("\t${v}") }
     }
 
 
-    fun pjClass(wire : Wire) : PJClass {
+    fun getPJClass(wire: Wire): PJClass {
         val wn = wire.wireName
         when {
             wn.contains("CLK") -> return PJClass.CLK
             wn.contains("VCC") || wn.contains("GND") -> return PJClass.ELEC
-            else -> return PJClass.CONN
+            else -> return PJClass.ROUTING
         }
     }
 
-    fun pjOfClass(t : PJClass)  =  pipJunctions.filter { pj -> pjClass(pj) == t }
+    fun pjOfClass(t: PJClass) = pipJunctions.filter { pj -> getPJClass(pj) == t }
 
-    fun pjType(pj : Wire) : PJType {
+    fun getPJType(pj: Wire): PJType {
         val bkPips = pj.backwardPIPs
         val fwPips = pj.forwardPIPs
         when {
@@ -94,75 +96,66 @@ class SwitchBoxScraper {
         return PJType.SOURCE
     }
 
-    fun createPJGraphNode(wire : Wire) : MutableNode {
+    fun getPJRoutingType(pj: Wire): PJRoutingType {
+        return PJRoutingType.GLOBAL
+    }
+
+    fun classifyPJ(pj: Wire): PJProperty {
+        var prop = PJProperty()
+        prop.pjClass = getPJClass(pj)
+        prop.pjType = getPJType(pj)
+        prop.routingType = getPJRoutingType(pj)
+        return prop
+    }
+
+    fun createPJGraphNode(wire: Wire): MutableNode {
         var node = mutNode(wire.wireName)
+        val prop = pjClassification[wire]
         // Color classification by PIP junction class
-        when {
-            pjClass(wire) == PJClass.ELEC -> node.add(Color.RED).add("fillcolor", "red2").add("style", "filled")
-            pjClass(wire) == PJClass.CLK -> node.add(Color.YELLOW).add("fillcolor", "yellow2").add("style", "filled")
-            else -> node.add(Color.BLACK).add("fillcolor", "gray").add("style", "filled")
+        when (prop?.pjClass) {
+            PJClass.ELEC -> node.add(Color.RED).add(Color.RED2.fill())
+            PJClass.CLK -> node.add(Color.YELLOW).add(Color.YELLOW2.fill())
+            else -> node.add(Color.BLACK).add(Color.GREY.fill())
         }
 
         // Shape classification by PIP junction type
-        when {
-            pjType(wire) == PJType.SOURCE -> node.add(Shape.RECTANGLE)
-            pjType(wire) == PJType.SINK -> node.add(Shape.DIAMOND)
+        when (prop?.pjType) {
+            PJType.SOURCE -> node.add(Shape.RECTANGLE)
+            PJType.SINK -> node.add(Shape.DIAMOND)
         }
 
         return node
     }
 
-    fun dotsToSvg(files : List<String>) : List<String> {
-        for(f in files) {
-            Runtime.getRuntime().exec("dot -Ksfdp -Tsvg -Goverlap=scale -O ${f}")
-        }
-        return files.map {file -> file + ".svg"}
+    fun dotsToSvg(files: List<String>): List<String> {
+        files.map { f -> Runtime.getRuntime().exec("dot -Ksfdp -Tsvg -Goverlap=scale -O ${f}") }
+        return files.map { file -> file + ".svg" }
     }
 
-    fun createDotGraph(includes : EnumSet<PJClass>) : String {
-        val includeString = includes.fold(""){str, v -> str + v}
+    fun createDotGraph(includes: EnumSet<PJClass>): String {
+        val includeString = includes.fold("") { str, v -> str + v }
         println("\tCreating graph for PIPs of type(s): ${includeString}")
         var pjNodes = mutableMapOf<Wire, MutableNode>()
 
-        for(pj in pipJunctions) {
-            val wireName = pj.wireName
-            if(pjClass(pj) == PJClass.CLK) {
-                if(!includes.contains(PJClass.CLK)) {
-                    // Clock wires excluded
-                    continue
-                }
-            } else if (pjClass(pj) == PJClass.ELEC) {
-                if (!includes.contains(PJClass.ELEC)) {
-                    // Electrical wires excluded
-                    continue
-                }
-            } else if (!includes.contains(PJClass.CONN)) {
-                // Normal routing wires excluded
-                continue
-            }
-            pjNodes[pj] = createPJGraphNode(pj)
+        // Create graph nodes for each pip junction class included in this graph
+        pipJunctions.filter { pj -> pjClassification[pj]?.pjClass in includes }.map { pj -> pjNodes[pj] = createPJGraphNode(pj) }
+
+        // Create directed links between all created graph nodes and their destination (Forward) PIPs also present in
+        // the graph.
+        pjNodes.map { pjn ->
+            pjn.key.forwardPIPs.filter { pip -> pjNodes.containsKey(pip.endWire) }.map { pip -> pjn.value.addLink(pjNodes[pip.endWire]) }
         }
 
-        for(pj in pipJunctions) {
-            for(toWire in pj.forwardPIPs) {
-                var toPJ = toWire.endWire
-                if(pjNodes.containsKey(toPJ)) {
-                    pjNodes[pj]?.addLink(pjNodes[toPJ])
-                }
-            }
-        }
+        // Create graph and add all created nodes
+        val graph = mutGraph(name).setDirected(true).nodeAttrs().add(Style.FILLED)
+        pjNodes.map { node -> graph.add(node.value) }
 
-        // Add nodes to graph
-        val graph = mutGraph(name).setDirected(true)
-        for(node in pjNodes) {
-            graph.add(node.value)
-        }
-
-        val fileName =  "graphs/${name}_${includeString}.dot"
+        val fileName = "graphs/${name}_${includeString}.dot"
         return Graphviz.fromGraph(graph).render(Format.DOT).toFile(File(fileName)).absolutePath
     }
 }
 
 fun main(args: Array<String>) {
-    val test = SwitchBoxScraper("xc7a35t")
+    val sbs = SwitchBoxScraper("xc7a35t")
+    sbs.scrape("INT_R_X41Y61")
 }
